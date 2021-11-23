3472b25c9a7c3119b9803c6260fcbc96a2154d67
pedroafleite <pedroaug.fl@gmail.com>
2021-11-23 20:02:08 -0300

    working greeting

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.4",
+  paths: {
+    artifacts: ".src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // ropsten: {
+    //   url: "https://ropsten.infura.io/v3/projectid",
+    //   accounts: [process.env.a2key]
+    // },
+    // rinkeby: {
+    //   url: "https://rinkeby.infura.io/v3/projectid",
+    //   accounts: [process.env.a2key]
+    // }
+  },
+  solidity: "0.8.3",
 };
